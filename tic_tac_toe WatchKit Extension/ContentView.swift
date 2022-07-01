635f69ba6e0df0aed1215aff2f3d8cb3bcc64339
caoziguang <caoziguang@bytedance.com>
2022-07-01 16:29:09 +0800

    feat: add base view

--- tic_tac_toe WatchKit Extension/ContentView.swift
+++ tic_tac_toe WatchKit Extension/ContentView.swift
@@ -7,10 +7,56 @@
 
 import SwiftUI
 
+var current = "X"
+func changeCurrent(current :String) -> String {
+    switch current{
+    case "X":
+        return "O"
+    default:
+        return "X"
+    }
+}
+struct Cell: View {
+    var id = UUID()
+    @State var value = ""
+    var body: some View {
+        Button(value,action: self.trigger)
+    }
+    func trigger(){
+        switch value{
+        case "":
+            value = current
+            current = changeCurrent(current: current)
+        default:
+            if(value != current){
+                current = value
+                value = ""
+            }
+        }
+        return
+    }
+}
+
+
+
+var panel = [
+    [Cell(), Cell(), Cell()],
+    [Cell(), Cell(), Cell()],
+    [Cell(), Cell(), Cell()],
+]
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        VStack {
+            Text("Tic Tac Toe").font(.title3)
+            ForEach(0..<panel.count,id:\.self) { i in
+                HStack {
+                    ForEach(panel[i],id: \.id) { cell in
+                        cell
+                    }
+                }
+            }
+        }
     }
 }
 
